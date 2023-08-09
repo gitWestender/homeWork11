1776d9335646c03c474bf2089a28b7b97b5437f8
Westender <javaxwestender@gmail.com>
2023-08-09 15:50:04 +0300

    Add: task2

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -14,7 +15,19 @@ public class Main {
         System.out.println("Введите год: ");
 
         int year = scan.nextInt();
+
         isLeapYear(year);
+    }
+
+    public static void task2() {
+        System.out.printf("\nЗадание %d:\n", counter++);
+        System.out.println("Введите OS и год выпуска вашего телефона: "
+                + "\nOS вида iOS или Android");
+
+        String clientOS = scan.nextLine();
+        int manufactureDate = scan.nextInt();
+
+        whichVersion(clientOS, manufactureDate);
     }
 
     static void isLeapYear(int year) {
@@ -24,4 +37,26 @@ public class Main {
             System.out.println(year + " год не является високосным");
         }
     }
+
+    static void whichVersion(String clientOS, int manufactureDate) {
+        switch (clientOS) {
+            case "iOS":
+                if (manufactureDate < 2015) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                }
+                break;
+            case "Android":
+                if (manufactureDate < 2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+                break;
+            default:
+                System.out.println("Под вашу операционную систему нечего скачивать");
+        }
+    }
+
 }
