48a58152c78f434eafae5d3c2aaa4e29665abb91
Westender <javaxwestender@gmail.com>
2023-08-09 15:34:44 +0300

    Add: task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+    static int counter = 1;
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+        System.out.printf("\nЗадание %d:\n", counter++);
+        System.out.println("Введите год: ");
+
+        int year = scan.nextInt();
+        isLeapYear(year);
+    }
+
+    static void isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
